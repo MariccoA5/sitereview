7ad3d8e609a687a5815ea0ad8e82ead5b7d10c41
MELO <MariccoA5@gmail.com>
2024-09-29 23:24:31 -0600

    stable with supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:site_connect/take_picture.dart';
 import 'package:site_connect/at_site_closeout.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://wtzgjzinkqbxnierobum.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0emdqemlua3FieG5pZXJvYnVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2NzI4NDUsImV4cCI6MjA0MzI0ODg0NX0.dBzXj8ULNUenZSFAb6MsSjp9rksCVM_pB476XtQMVjU',
+  );
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
-  runApp(MyApp(camera: firstCamera));
+  runApp(MyApp(camera: firstCamera,));
 }
 
 class MyApp extends StatelessWidget {
